24164505bd8b97b0258e5972f2268aa068d21884
Sonnyhyun <tjrgus4718@naver.com>
2025-09-08 23:09:13 +0900

    Initial commit: Add python mini projects

---
+++ project - 2.py
@@ -0,0 +1,74 @@
+import requests
+from datetime import datetime
+
+GENDER = "man"
+WEIGHT_KG = "74"
+HEIGHT_CM = "177"
+AGE = "23"
+
+
+API_ID = "f72e7d31"
+API_KEY = "fde65db7aaf83d374f13ee0d25cc94cb"
+
+
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheet_endpoint = "https://api.sheety.co/a578bdd9a2a0a797fc4e8383da9193a5/myWorkouts/workouts"
+
+
+exercise_text = input("Tell me which exercises you did: ")
+
+headers = {
+    "x-app-id": API_ID,
+    "x-app-key": API_KEY,
+    }
+
+params = {
+    "query": exercise_text,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
+}
+
+response = requests.post(url=exercise_endpoint, json=params, headers=headers)
+result = response.json()
+
+
+today_date = datetime.now().strftime('%Y%m%d')
+now_time = datetime.now().strftime("%X")
+
+for exercise in result["exercises"]:    #🤯🤯🤯이 부분부터 다시 이해 🤯🤯🤯 
+    sheet_input = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise":	exercise["name"].title(),
+            "duration":	exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+
+        }
+    }
+
+    sheet_response = requests.post(sheet_endpoint, json=sheet_input)
+    print(sheet_response)
+
+    sheet_response = requests.post(
+        sheet_endpoint, 
+        json=sheet_input, 
+        auth=(
+            "sonnyhyunny", 
+            "tjrgus8244",
+        )
+    )
+
+    #Bearer Token Authentication
+    bearer_headers = {
+    "Authorization": f"Bearer {"sadasdevfvwedcfwxcc"}"
+    }
+    sheet_response = requests.post(
+        sheet_endpoint, 
+        json=sheet_input, 
+        headers=bearer_headers
+    )
+
+    print(sheet_response.text)
---
+++ project - 3.py
@@ -0,0 +1,53 @@
+import requests
+import spotipy
+import pprint
+from spotipy.oauth2 import SpotifyOAuth
+from bs4 import BeautifulSoup
+
+CLIENT_ID = "80ab4457fcb14e54909949a17cccd000"
+CLIENT_SECRET = "a20c143231c644669851d6667ad69006"
+REDIRECT_URL = "http://example.com"
+
+response = requests.get("https://www.billboard.com/charts/hot-100/2000-08-12")
+web_page = response.text
+
+
+soup = BeautifulSoup(web_page, "html.parser")
+
+title = soup.select("li ul li h3")
+song_titles = [song.getText().strip() for song in title]
+
+
+
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        client_id = CLIENT_ID,
+        client_secret = CLIENT_SECRET,
+        redirect_uri = REDIRECT_URL,
+        scope = "playlist-modify-private",
+        cache_path = "DAY46/token.txt",
+        show_dialog = True
+    )
+)
+
+user_id = sp.current_user()['id']
+music_travel = input("음악여행 몇 년도로 갈래 YYYY-MM-DD?")
+song_names = ["The list of song", "titles from your", "web scrape"]
+
+song_url = []
+year = music_travel.split("-")[0]
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    print(result)
+    try:
+        url = result["tracks"]
+        song_url.append(url)
+        
+    except IndexError:
+        print(f"A song {song} that you want to hear is not founded, so skip")
+
+
+playlists = sp.user_playlist_create(user=user_id, name=f"{music_travel} Billboard 100", public=False)
+
+sp.playlist_add_items(playlist_id=playlists['id'], items=song_url)
